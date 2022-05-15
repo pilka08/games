5438180a4e1fdd92a110178c6962fcaececf0309
pilka08 <105580822+pilka08@users.noreply.github.com>
2022-05-15 09:41:58 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 537413528 then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/pilka08/games/main/boat'))()
+end
+
+if game.PlaceId == 3527629287 then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/pilka08/games/main/big'))()
+end
+
+if game.PlaceId == 2788229376 then
+    loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/pilka08/games/main/da'))()
+end
